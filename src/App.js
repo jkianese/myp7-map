5002c57981783b98b8898549cb561844ac65e1be
[jkianese] <[jkianese@gmail.com]>
2018-10-08 23:06:18 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MyMap from './components/MyMap'
+import FourSquare from './api/FourSquare'
 
 class App extends Component {
+
+  // set up constructor and state
+  constructor() {
+    super();
+    this.state = {
+      venues: [],
+      markers: [],
+      center: [],
+      zoom: 12
+    }
+  }  
+  closeAllMarkers = () => {
+    const markers = this.state.markers.map(marker => {
+      marker.isOpen = false;
+      return marker;
+    });
+    this.setState({ markers: Object.assign(this.state.markers, markers) })
+  };
+
+  handleMarkerClick = (marker) => {
+    this.closeAllMarkers();
+    console.log(marker)
+    marker.isOpen = true;
+    this.setState({markers: Object.assign(this.state.markers, marker)})
+
+    const venue = this.state.venues.find(venue => venue.id === marker.id);
+
+    FourSquare.getVenueDetails(marker.id).then(res => {
+      const newVenue = Object.assign(venue, res.response.venue);
+      this.setState({ venues: Object.assign(this.state.venues, newVenue) });
+      console.log(newVenue);
+    });  
+  }
+
+  componentDidMount(){
+    FourSquare.search({
+      near: "Pittsburgh",
+      query: "food",
+      limit: 10
+    }).then(results => {
+      const { venues } = results.response;
+      const { center } = results.response.geocode.feature.geometry;
+      const markers = venues.map(venue => {
+        return {
+          lat: venue.location.lat,
+          lng: venue.location.lng,
+          isOpen: false,
+          isVisible: true,
+          id: venue.id
+        };
+      });
+      this.setState({ venues, center, markers });
+        console.log(results);
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <MyMap { ...this.state} handleMarkerClick={this.handleMarkerClick} />
+      </div>  
     );
   }
 }
---
+++ src/components/MyMap.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react'
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps"
+
+const MyMapComponent = withScriptjs(
+    withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={12}
+    zoom={props.zoom}
+    defaultCenter={{ lat: 40.448506, lng: -80.00250 }}
+    center={props.center}
+  >  
+    {props.markers && 
+        props.markers.filter(marker => marker.isVisible).map((marker, idx) => { 
+            const venueInfo = props.venues.find(venue => venue.id === marker.id);
+                return (
+                    <Marker 
+                      key={idx} 
+                      position={{ lat: marker.lat, lng: marker.lng }}
+                      onClick={() => props.handleMarkerClick(marker)}
+                    >
+                {marker.isOpen &&  
+                    venueInfo.bestPhoto && (
+                <InfoWindow>
+                    <React.Fragment>
+                        <img src={`${venueInfo.bestPhoto.prefix}100x100${venueInfo.bestPhoto.suffix}`} alt={"P"}/>
+                      <p>{venueInfo.name}</p>  
+                    </React.Fragment>
+                </InfoWindow>
+                    
+                )}
+            </Marker>    
+            );
+                })}       
+  </GoogleMap>
+));
+
+
+export default class MyMap extends Component {
+    render() {
+        return (
+            <MyMapComponent
+                {...this.props}
+                isMarkerShown  // Appears like this was remove Video #4 3:50 
+                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBj5AzHYC1kUPRnvaT6G6zsAONHSpKmoqQ"
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `400px` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+            />
+        )        
+    }    
+}
+
+
+
